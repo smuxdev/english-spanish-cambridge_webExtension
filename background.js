bb6641219dd9799b6f352c60e5739e70343b4f68
samuelgonzalez-stratio <106381336+samuelgonzalez-stratio@users.noreply.github.com>
2022-11-15 15:38:32 +0000

    Now, dictionary popup close when focus is lost.

--- background.js
+++ background.js
@@ -1,16 +1,34 @@
 // Listen to "content-script" messages
 browser.runtime.onMessage.addListener(openDictionary);
 
+function onCreated(windowInfo) {
+  browser.windows.onFocusChanged.addListener(e =>
+    browser.windows.remove(windowInfo.id)
+  );
+}
+
+function onError(error) {
+  console.log(`Error: ${error}`);
+}
+
 /*
 Open a panel with the Cambridge dictionary spanish-english
 */
 function openDictionary (selectedText) {
-   var url = "https://dictionary.cambridge.org/dictionary/english-spanish/" + selectedText;
 
-   let creating = browser.windows.create({
-     url: url,
-     type: "popup",
-     height: 600,
-     width: 400
-   });
+  if(selectedText) {
+     var url = "https://dictionary.cambridge.org/dictionary/english-spanish/" + selectedText;
+
+     let windowDic = browser.windows.create({
+       url: url,
+       type: "popup",
+       height: 600,
+       width: 400
+     });
+
+    windowDic.then(onCreated, onError);
+
+  }
+
 }
+
